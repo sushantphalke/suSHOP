e69f2d20c4496ccb1b9402e3e0a78fc5879e30aa
sushantphalke <77383095+SushantPhalke@users.noreply.github.com>
2023-04-28 20:08:04 +0530

    suSHop

--- src/components/topbar.js
+++ src/components/topbar.js
@@ -43,7 +43,7 @@ const Topbar = () => {
                         ms='2'
                         title='logo'
                     >
-                        vaiSH
+                        suSHOP
                     </Link>
                 </Flex>
 
